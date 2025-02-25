331bd0dd45ddc4f2f807e2897ee6376af00534c1
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2025-02-25 19:13:36 +0300

    Fix compileSdk & targetSdk version (35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.yandex.practicum.sprint8koh27"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.yandex.practicum.sprint8koh27"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
